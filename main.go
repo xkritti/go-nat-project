8ef1e9d5547e5d6f08a70e731c456fa81b4754a0
Donnukrit Satirakul <osacn_donnukrit.satirakul@krungthai.com>
2023-09-26 17:33:11 +0700

    update main

--- main.go
+++ main.go
@@ -14,21 +14,6 @@ import (
 	"github.com/gofiber/swagger"
 )
 
-// @title Swagger Example APIS
-// @version 1.0
-// @description This is a sample server Petstore server.
-// @termsOfService http://swagger.io/terms/
-
-// @contact.name API Support
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
-
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host petstore.swagger.io
-// @BasePath /v2
-
 func main() {
 
 	database.Connect()
