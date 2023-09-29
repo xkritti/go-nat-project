3fca471a3abd961649e2b508bcb846da6a6273ea
xkritti <krittamet0404@gmail.com>
2023-09-29 15:04:14 +0700

    ✨ feat(utils): ExcelReader

--- utils/utils.go
+++ utils/utils.go
@@ -6,6 +6,7 @@ import (
 	"go-nat-project/models"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/xuri/excelize/v2"
 )
 
 func SendSuccess(c *fiber.Ctx, data interface{}) error {
@@ -25,4 +26,23 @@ func GetSha256Enc(text string) string {
 	bs := h.Sum(nil)
 	result := fmt.Sprintf("%x", bs)
 	return result
+}
+
+func ExcelReader(filename string, index int) (*excelize.File, int, string, error) {
+	excelResult, err := excelize.OpenFile(filename)
+	if err != nil {
+		return nil, 0, "", err
+	}
+
+	file, err := excelize.OpenFile(filename)
+	if err != nil {
+		return nil, 0, "", err
+	}
+
+	rows, err := file.GetRows(file.GetSheetName(index))
+	if err != nil {
+		return nil, 0, "", err
+	}
+
+	return excelResult, len(rows), "", nil
 }
