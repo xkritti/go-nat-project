30532291993be065ab33e62513648ab5d916cf26
xkritti <krittamet0404@gmail.com>
2023-09-29 16:40:19 +0700

    🐞 fix(utils):
    add deletefile and fix readfile

--- utils/utils.go
+++ utils/utils.go
@@ -2,8 +2,11 @@ package utils
 
 import (
 	"crypto/sha256"
+	"errors"
 	"fmt"
 	"go-nat-project/models"
+	"io"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/xuri/excelize/v2"
@@ -28,21 +31,59 @@ func GetSha256Enc(text string) string {
 	return result
 }
 
-func ExcelReader(filename string, index int) (*excelize.File, int, string, error) {
+func ExcelReader(filename string, index int) (*excelize.File, string, int, error) {
 	excelResult, err := excelize.OpenFile(filename)
+	sheetName := excelResult.GetSheetName(index)
 	if err != nil {
-		return nil, 0, "", err
+		return nil, "", 0, err
 	}
 
 	file, err := excelize.OpenFile(filename)
 	if err != nil {
-		return nil, 0, "", err
+		return nil, "", 0, err
 	}
 
 	rows, err := file.GetRows(file.GetSheetName(index))
 	if err != nil {
-		return nil, 0, "", err
+		return nil, "", 0, err
 	}
 
-	return excelResult, len(rows), "", nil
+	return excelResult, sheetName, len(rows), nil
+}
+
+func UploadFileReader(c *fiber.Ctx) (string, error) {
+	file, err := c.FormFile("file")
+	if err != nil {
+		return "", errors.New("Uploading Failed!")
+	}
+
+	src, err := file.Open()
+	if err != nil {
+		return "", errors.New("Source Invalid")
+	}
+
+	defer src.Close()
+
+	dest, err := os.Create(file.Filename)
+
+	defer dest.Close()
+
+	_, err = io.Copy(dest, src)
+
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println("Upload Excel")
+
+	return file.Filename, nil
+}
+
+func DeleteFile(filename string) error {
+	fmt.Println(filename)
+	err := os.Remove(filename)
+	if err != nil {
+		return err
+	}
+	return nil
 }
